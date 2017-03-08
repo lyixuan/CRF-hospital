1ac0b7788c96a0721cbf9bae47ca349dfff52dd3
yixuan <1726801110@qq.com>
2017-03-08 17:45:35 +0800

    登录调整

--- config/index.js
+++ config/index.js
@@ -5,7 +5,6 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
-    login: path.resolve(__dirname, '../dist/login.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
